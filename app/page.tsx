8f3dfa0c5b1168aeae503203db431eaa9196714b
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-02-01 14:13:17 +0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
+import Hero from "./components/Hero";
 import Newest from "./components/Newest";
-import Hero from "./components/hero";
 
 export default function Home() {
   return (
